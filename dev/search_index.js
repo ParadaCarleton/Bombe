b28fd3e7fe8e7e740a61e935a64bc0af57ee260c
Documenter.jl <documenter@juliadocs.github.io>
2021-07-05 03:55:41 +0000

    build based on 45b0402

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Bombe","category":"page"},{"location":"#Bombe","page":"Home","title":"Bombe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bombe.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Bombe]","category":"page"}]
+}
